52b2dc9e48e22f4f2b368b7d6085e183466ca734
piyush33 <124145293+piyushOrigamy@users.noreply.github.com>
2023-04-05 18:07:51 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
-import LoginOtp from "./components/loginOtp";
-
-
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/login" element ={<Login/>}/>
-      <Route path="/login-otp" element ={<LoginOtp/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
